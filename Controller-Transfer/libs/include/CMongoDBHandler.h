8b54ef630785a6dad9b54213e0a30f1378847be0
ideasiii <louisju@iii.org.tw>
2017-01-17 17:28:08 +0800

    update

---
+++ Controller-Transfer/libs/include/CMongoDBHandler.h
@@ -0,0 +1,34 @@
+/*
+ * CMongoDBHandler.h
+ *
+ *  Created on: 2015年12月01日
+ *      Author: Louis Ju
+ */
+
+#pragma once
+#include <string>
+#include <map>
+#include <list>
+
+class CMongoDBHandler
+{
+public:
+	static CMongoDBHandler* getInstance();
+	virtual ~CMongoDBHandler();
+
+	int connectDB();
+	int connectDB(std::string strIP, std::string strPort);
+	int connectDB(std::string strIP, std::string strPort, std::string strDBName, std::string strUser,
+			std::string strPasswd);
+	void close();
+	void insert(std::string strDB, std::string strCollection, std::map<std::string, std::string> &mapData);
+	void insert(std::string strDB, std::string strCollection, std::string strColumn, std::string strValue);
+	std::string insert(std::string strDB, std::string strCollection, std::string strJSON);
+	int query(std::string strDB, std::string strCollection, std::string strField, std::string strCondition,
+			std::list<std::string> &listJSON);
+	bool isValid();
+
+private:
+	explicit CMongoDBHandler();
+	static CMongoDBHandler* mInstance;
+};
