8a8717d8cc5d6e1923356e9de77c073dc828dc2a
ideasiii <louisju@iii.org.tw>
2017-04-08 18:22:51 +0800

    update

--- Controller-Semantic/controllerHandler/CController.h
+++ Controller-Semantic/controllerHandler/CController.h
@@ -1,63 +1,21 @@
 /*
  * CController.h
  *
- *  Created on: 2016年06月27日
+ *  Created on: 2017年4月8日
  *      Author: Jugo
  */
 
 #pragma once
 
-#include <string>
-#include <vector>
-#include <list>
-#include "CObject.h"
-#include "common.h"
+#include "CApplication.h"
 
-#define MAX_FUNC_POINT		256
-
-class CSocketServer;
-class CSocketClient;
-class CCmpHandler;
-class CSqliteHandler;
-class CThreadHandler;
-class CJsonHandler;
-
-class CController: public CObject
+class CController: public CApplication
 {
 public:
+	CController();
 	virtual ~CController();
-	static CController* getInstance();
-	int startServer(const int nPort, const int nMsqId);
-	void stopServer();
-	void runEnquireLinkRequest();
-	int startSqlite(const int nDBId, const std::string strDB);
-	int startMongo(const std::string strIP, const int nPort);
 
 protected:
-	void onReceiveMessage(int nEvent, int nCommand, unsigned long int nId, int nDataLen, const void* pData);
-
-private:
-	explicit CController();
-	void onCMP(int nClientFD, int nDataLen, const void *pData);
-	int sendCommand(int nSocket, int nCommand, int nStatus, int nSequence, bool isResp);
-	void ackPacket(int nClientSocketFD, int nCommand, const void * pData);
-
-	/**  Receive CMP Request **/
-	int cmpUnknow(int nSocket, int nCommand, int nSequence, const void * pData);
-	int cmpSemantic(int nSocket, int nCommand, int nSequence, const void * pData);
-
-	/** Send CMP Response **/
-	int cmpResponse(const int nSocket, const int nCommandId, const int nSequence, const char * szData = 0);
-
-private:
-	CSocketServer *cmpServer;
-	CCmpHandler *cmpParser;
-	CSqliteHandler *sqlite;
-
-	std::vector<int> vEnquireLink;
-	CSocketClient *cmpClient;
-
-	typedef int (CController::*MemFn)(int, int, int, const void *);
-	MemFn cmpRequest[MAX_FUNC_POINT];
+	void onInitial();
 
 };
--- Controller-Semantic/modules.mk
+++ Controller-Semantic/modules.mk
@@ -1,16 +1,17 @@
-export MODULES=global_inc \
-			 main \
-			 controllerHandler \
-			 configHandler	\
-			 messageHandler	\
-			 objectHandler	\
-			 socketHandler	\
-			 threadHandler	\
-			 dataHandler	\
-			 cmpHandler	\
-			 sqliteHandler	\
-			 jsonHandler \
-			 logHandler \
-			 processHandler
+export MODULES=controllerHandler \
+../Global/application \
+../Global/global_inc \
+../Global/configHandler	\
+../Global/messageHandler	\
+../Global/objectHandler	\
+../Global/socketHandler	\
+../Global/threadHandler	\
+../Global/dataHandler	\
+../Global/jsonHandler \
+../Global/logHandler \
+../Global/processHandler \
+../Global/cmpHandler \
+../Global/mysqlHandler \
+../Global/protocolHandler
 			 
 #export EXT_MODULES=extInclude
