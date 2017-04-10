ca83b71921c2024344da3fbba79a8e9d84af1f7c
ideasiii <louisju@iii.org.tw>
2017-04-10 17:35:54 +0800

    add

---
+++ Controller-Semantic/controllerHandler/CCmpWord.cpp
@@ -0,0 +1,20 @@
+/*
+ * CCmpWord.cpp
+ *
+ *  Created on: 2017年4月10日
+ *      Author: root
+ */
+
+#include "CCmpWord.h"
+
+CCmpWord::CCmpWord()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CCmpWord::~CCmpWord()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Controller-Semantic/controllerHandler/CCmpWord.h
@@ -0,0 +1,17 @@
+/*
+ * CCmpWord.h
+ *
+ *  Created on: 2017年4月10日
+ *      Author: root
+ */
+
+#pragma once
+
+#include "CCmpServer.h"
+
+class CCmpWord: public CCmpServer
+{
+public:
+	explicit CCmpWord();
+	virtual ~CCmpWord();
+};
