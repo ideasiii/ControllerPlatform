981182107548ffca43375a7cad9020079c141cb4
ideasiii <louisju@iii.org.tw>
2017-04-19 16:16:50 +0800

    update

--- Controller-Semantic/controllerHandler/CSemanticJudge.cpp
+++ Controller-Semantic/controllerHandler/CSemanticJudge.cpp
@@ -35,10 +35,12 @@ void CSemanticJudge::word(const char *szInput, JSONObject *jsonResp)
 
 	if(string::npos != strWord.find("故事"))
 	{
+		JSONObject jsonStory;
+		jsonStory.put("host", STORY_HOST);
+		jsonStory.put("story", "三隻小豬.mp3");
+		jsonStory.put("title", "三隻小豬");
 		jsonResp->put("type", TYPE_RESP_STORY);
-		jsonResp->put("host", STORY_HOST);
-		jsonResp->put("story", "三隻小豬.mp3");
-		jsonResp->put("title", "三隻小豬");
+		jsonResp->put("story", jsonStory);
 	}
 }
 
