782d0e609673c6f33193c47791a31fd2e046dc45
ideasiii <louisju@iii.org.tw>
2017-04-21 15:54:57 +0800

    update

--- Controller-Semantic/controllerHandler/music.h
+++ Controller-Semantic/controllerHandler/music.h
@@ -14,7 +14,7 @@ using namespace std;
 
 //======================== Local Mood Music ==============================//
 // ("歡喜" || "忿怒" || "哀傷" || "驚恐" || "愛情")
-static map<string, string> mapMood =
-		create_map<string, string>\
-("歡喜", "happy.mp3")("忿怒", "angry.mp3")("哀傷", "sad.mp3")("驚恐", "shock.mp3")("愛情",
-				"love.mp3");
+static map<string, string> mapMood = create_map<string, string>\
+("歡喜", "happy.mp3")("忿怒", "angry.mp3")("憤怒",
+		"angry.mp3")("氣憤", "angry.mp3")("不爽", "angry.mp3")("哀傷", "sad.mp3")("悲傷", "sad.mp3")("傷感", "sad.mp3")("驚恐",
+		"shock.mp3")("愛情", "love.mp3");
