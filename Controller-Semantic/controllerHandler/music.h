28f0f8a2d44ab1602e3620e456ea834a9dd4cb3e
ideasiii <louisju@iii.org.tw>
2017-04-21 15:21:02 +0800

    update

--- Controller-Semantic/controllerHandler/music.h
+++ Controller-Semantic/controllerHandler/music.h
@@ -7,32 +7,10 @@
 
 #pragma once
 
+#include "create_map.h"
 #include <string>
 #include <map>
 using namespace std;
-
-template<typename T, typename U>
-class create_map
-{
-private:
-	std::map<T, U> m_map;
-public:
-	create_map(const T& key, const U& val)
-	{
-		m_map[key] = val;
-	}
-
-	create_map<T, U>& operator()(const T& key, const U& val)
-	{
-		m_map[key] = val;
-		return *this;
-	}
-
-	operator std::map<T, U>()
-	{
-		return m_map;
-	}
-};
 
 //======================== Local Mood Music ==============================//
 // ("歡喜" || "忿怒" || "哀傷" || "驚恐" || "愛情")
--- Controller-Semantic/controllerHandler/story.h
+++ Controller-Semantic/controllerHandler/story.h
@@ -7,33 +7,10 @@
 
 #pragma once
 
+#include "create_map.h"
 #include <string>
 #include <map>
 using namespace std;
-
-template<typename T, typename U>
-class create_map
-{
-private:
-	std::map<T, U> m_map;
-public:
-	create_map(const T& key, const U& val)
-	{
-		m_map[key] = val;
-	}
-
-	create_map<T, U>& operator()(const T& key, const U& val)
-	{
-		m_map[key] = val;
-		return *this;
-	}
-
-	operator std::map<T, U>()
-	{
-		return m_map;
-	}
-
-};
 
 static map<string, string> mapStory = create_map<string, string>\
 ("三隻小豬", "三隻小豬.mp3")("小美人魚", "小美人魚.mp3")("美人魚",
