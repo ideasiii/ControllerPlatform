b3f31e2607e3690e4071a77e1f01553ed82c3298
ideasiii <louisju@iii.org.tw>
2017-04-25 14:05:02 +0800

    update

---
+++ Controller-Semantic/controllerHandler/dic_artist.h
@@ -0,0 +1,15 @@
+/*
+ * artist.h
+ *
+ *  Created on: 2017年4月25日
+ *      Author: root
+ */
+
+#ifndef CONTROLLERHANDLER_DIC_ARTIST_H_
+#define CONTROLLERHANDLER_DIC_ARTIST_H_
+
+
+
+
+
+#endif /* CONTROLLERHANDLER_DIC_ARTIST_H_ */
