07a984fac79643f182e860c4c5e1425edab576d8
ideasiii <louisju@iii.org.tw>
2018-06-27 14:21:35 +0800

    update_jugo

--- Controller-Semantic/judgeHandler/CJudgeStory.cpp
+++ Controller-Semantic/judgeHandler/CJudgeStory.cpp
@@ -19,6 +19,7 @@
 
 #define STORY_PATH					"/data/opt/tomcat/webapps/story/"
 #define HOST_MOOD					"https://ryejuice.sytes.net/edubot/mood/"
+#define FILE_TYPE_STORY				"ogg"
 
 using namespace std;
 
@@ -132,22 +133,22 @@ void CJudgeStory::loadStoryDictionary()
 	int nIndex;
 	CFileHandler fh;
 	set<string> setData;
-	set<string> setMood;
+	//set<string> setMood;
 	set<string>::const_iterator iter;
-	set<string>::const_iterator iter_set_mood;
+	//set<string>::const_iterator iter_set_mood;
 	string strFileType;
 	string strWord;
 	string strFile;
 	string strFileName;
 	string strTime;
-	string strMood;
-	JSONObject jobjMood;
+	//string strMood;
+	//JSONObject jobjMood;
 	extern map<string, string> mapStory;
-	extern map<string, JSONArray> mapStoryMood;
+	//extern map<string, JSONArray> mapStoryMood;
 
 	fh.readPath(STORY_PATH, setData);
 	mapStory.clear();
-	mapStoryMood.clear();
+	//mapStoryMood.clear();
 
 	for(iter = setData.begin(); setData.end() != iter; ++iter)
 	{
@@ -155,42 +156,51 @@ void CJudgeStory::loadStoryDictionary()
 		if((int) string::npos != nIndex)
 		{
 			strFileType = iter->substr(nIndex + 1);
-			if(!strFileType.compare("mp3") || !strFileType.compare("mood"))
+			if(!strFileType.compare(FILE_TYPE_STORY))
 			{
 				strFileName = iter->substr(0, nIndex);
-				if(strFileType.compare("mood"))
-					mapStory[strFileName] = iter->c_str();
-
-				if(!strFileType.compare("mood"))
-				{
-					setMood.clear();
-					if(0 < fh.readAllLine(string(STORY_PATH + (*iter)).c_str(), setMood))
-					{
-						mapStoryMood[strFileName + ".mp3"].create();
-						for(iter_set_mood = setMood.begin(); setMood.end() != iter_set_mood; ++iter_set_mood)
-						{
-							if(!iter_set_mood->empty())
-							{
-								nIndex = iter_set_mood->find(",");
-								strTime = iter_set_mood->substr(0, nIndex);
-								strMood = iter_set_mood->substr(nIndex + 1);
-								jobjMood.create();
-								convertFromString(nTime, strTime);
-								jobjMood.put("time", nTime);
-								jobjMood.put("host", HOST_MOOD);
-								jobjMood.put("file", strMood);
-								jobjMood.put("description", strMood);
-								mapStoryMood[trim(strFileName + ".mp3")].add(jobjMood);
-								jobjMood.release();
-							}
-						}
-					}
-				}
+				mapStory[strFileName] = iter->c_str();
 			}
+			/*
+			 if(!strFileType.compare(FILE_TYPE_STORY) || !strFileType.compare("mood"))
+			 {
+			 strFileName = iter->substr(0, nIndex);
+
+			 if(strFileType.compare("mood"))
+			 mapStory[strFileName] = iter->c_str();
+
+			 if(!strFileType.compare("mood"))
+			 {
+			 setMood.clear();
+			 if(0 < fh.readAllLine(string(STORY_PATH + (*iter)).c_str(), setMood))
+			 {
+			 mapStoryMood[strFileName + ".mp3"].create();
+			 for(iter_set_mood = setMood.begin(); setMood.end() != iter_set_mood; ++iter_set_mood)
+			 {
+			 if(!iter_set_mood->empty())
+			 {
+			 nIndex = iter_set_mood->find(",");
+			 strTime = iter_set_mood->substr(0, nIndex);
+			 strMood = iter_set_mood->substr(nIndex + 1);
+			 jobjMood.create();
+			 convertFromString(nTime, strTime);
+			 jobjMood.put("time", nTime);
+			 jobjMood.put("host", HOST_MOOD);
+			 jobjMood.put("file", strMood);
+			 jobjMood.put("description", strMood);
+			 mapStoryMood[trim(strFileName + ".mp3")].add(jobjMood);
+			 jobjMood.release();
+			 }
+			 }
+			 }
+			 }
+			 }
+			 */
 		}
 	}
 
 	setData.clear();
+	// 模糊語意偵測
 	fh.readAllLine("dictionary/match_file_story.txt", setData);
 	for(iter = setData.begin(); setData.end() != iter; ++iter)
 	{
@@ -204,9 +214,9 @@ void CJudgeStory::loadStoryDictionary()
 	}
 
 	_log("[CJudgeStory] loadStoryDictionary Load Story: %d", mapStory.size());
-	_log("[CJudgeStory] loadStoryDictionary Load Story Mood: %d", mapStoryMood.size());
-//	for(map<string, string>::const_iterator it = mapStory.begin(); mapStory.end() != it; ++it)
-//		_log("%s - %s", it->first.c_str(), it->second.c_str());
+//	_log("[CJudgeStory] loadStoryDictionary Load Story Mood: %d", mapStoryMood.size());
+	for(map<string, string>::const_iterator it = mapStory.begin(); mapStory.end() != it; ++it)
+		_log("%s - %s", it->first.c_str(), it->second.c_str());
 
 //	for(map<string, JSONArray>::iterator it = mapStoryMood.begin(); mapStoryMood.end() != it; ++it)
 //		_log("%s - %s", it->first.c_str(), it->second.toString().c_str());
