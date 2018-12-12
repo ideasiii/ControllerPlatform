ca10b3ce468e7f7a0d3eba5fbadf2461501ec21d
jugo8633 <louisju@iii.org.tw>
2018-12-12 16:39:28 +0800

    update

---
+++ Controller-TTS/textToSpeech/textProcess/CSentence.cpp
@@ -0,0 +1,20 @@
+/*
+ * CSentence.cpp
+ *
+ *  Created on: 2018年12月10日
+ *      Author: root
+ */
+
+#include "CSentence.h"
+
+CSentence::CSentence()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CSentence::~CSentence()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Controller-TTS/textToSpeech/textProcess/CSentence.h
@@ -0,0 +1,16 @@
+/*
+ * CSentence.h
+ *
+ *  Created on: 2018年12月10日
+ *      Author: jugo
+ */
+
+#pragma once
+
+class CSentence
+{
+public:
+	explicit CSentence();
+	virtual ~CSentence();
+	void loadDictionary();
+};
