0752af9c2e9ebaeced98582bfeb4a58471a5d802
krischen <krischen@iii.org.tw>
2019-11-08 15:16:01 +0800

    new model 30

--- Controller-TTS/textToSpeech/textProcess/CTextProcess.cpp
+++ Controller-TTS/textToSpeech/textProcess/CTextProcess.cpp
@@ -101,6 +101,7 @@ static std::map<int, const char*> ModelMap = {
 		{26,  "model/hmm_26.htsvoice"},
 		{27,  "model/hmm_27.htsvoice"},
 		{28,  "model/hmm_28.htsvoice"},
+		{30,  "model/hmm_30.htsvoice"},
 		{101, "model/hmm_101.htsvoice"},
 		{102, "model/hmm_102.htsvoice"},
 		{103, "model/hmm_103.htsvoice"},
